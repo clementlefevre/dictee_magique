d644bf570f14918aba95fc1a6b632c7c4360a783
clementlefevre <clement.san@gmail.com>
2021-04-30 16:14:57 +0200

    ghjghj

--- update_bucket.sh
+++ update_bucket.sh
@@ -1,9 +1,3 @@
 pip install  --ignore-installed -r python/requirements.txt &&
-sudo apt-get install nodejs awscli &&
-python python/update_questions.py &&
-sudo chown -R $USER /usr/local/lib/node_modules/yarn &&
-sudo chown -R $USER /usr/local/lib/node_modules &&
-sudo chown -R $USER /usr/local/bin/yarnpkg &&
-npm install  yarn && 
-cd /home/runner/work/dictee_magique/app/package.json && yarn build && cd .. &&
-aws s3 sync app/dist s3://ladicteemagique
+sudo apt-get install awscli &&
+python python/update_questions.py 
