90f26f3b974c139eaeaea443069ba7635cf5e06e
clementlefevre <clement.san@gmail.com>
2021-04-30 16:04:57 +0200

    fghfgh

--- update_bucket.sh
+++ update_bucket.sh
@@ -4,6 +4,6 @@ python python/update_questions.py &&
 sudo chown -R $USER /usr/local/lib/node_modules/yarn &&
 sudo chown -R $USER /usr/local/lib/node_modules &&
 sudo chown -R $USER /usr/local/bin/yarnpkg &&
-npm install --global yarn && 
+npm install  yarn && 
 cd app && yarn build && cd .. &&
 aws s3 sync app/dist s3://ladicteemagique
