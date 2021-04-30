6017d974e630b66fce2093bb9ef4229977dce2fd
clementlefevre <clement.san@gmail.com>
2021-04-30 15:42:55 +0200

    fix req

--- update_bucket.sh
+++ update_bucket.sh
@@ -1,5 +1,5 @@
-sudo apt-get install nodejs awscli &&
 pip install -r python/requirements.txt &&
+sudo apt-get install nodejs awscli &&
 python python/update_questions.py &&
 npm install --global yarn && 
 cd app && yarn build && cd .. &&
