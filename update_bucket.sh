7847496ea37a7616bc4674bdc9001ae0ccb4d535
clementlefevre <clement.san@gmail.com>
2021-04-30 16:08:54 +0200

    fghfh

--- update_bucket.sh
+++ update_bucket.sh
@@ -5,5 +5,5 @@ sudo chown -R $USER /usr/local/lib/node_modules/yarn &&
 sudo chown -R $USER /usr/local/lib/node_modules &&
 sudo chown -R $USER /usr/local/bin/yarnpkg &&
 npm install  yarn && 
-cd app && yarn build && cd .. &&
+cd /home/runner/work/dictee_magique/app/package.json && yarn build && cd .. &&
 aws s3 sync app/dist s3://ladicteemagique
