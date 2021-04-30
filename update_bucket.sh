b80ff729a08aedf6ae6c4c192a062f8a3cd491d7
clementlefevre <clement.san@gmail.com>
2021-04-30 16:01:26 +0200

    klöklö

--- update_bucket.sh
+++ update_bucket.sh
@@ -3,6 +3,7 @@ sudo apt-get install nodejs awscli &&
 python python/update_questions.py &&
 sudo chown -R $USER /usr/local/lib/node_modules/yarn &&
 sudo chown -R $USER /usr/local/lib/node_modules &&
+sudo chown -R $USER /usr/local/bin/yarnpkg &&
 npm install --global yarn && 
 cd app && yarn build && cd .. &&
 aws s3 sync app/dist s3://ladicteemagique
